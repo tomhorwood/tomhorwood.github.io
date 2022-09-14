050b69da9a13c0c11cc41ea493d5bed5ff246c6a
Tom Horwood <tom.horwood@gomedia.io>
2022-09-14 15:12:04 +0100

    #main: Image comparison

---
+++ size-compare/imgs.js
@@ -0,0 +1,505 @@
+
+let images;
+images = [
+    {
+      "name": "CAP_BAY_00916993_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "32K",
+      "smaller": "11K",
+      "webp": "5.3K",
+      "original_bytes": 32418,
+      "smaller_bytes": "11376",
+      "webp_bytes": 5396
+    },
+    {
+      "name": "CAP_BAY_00916998_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "120K",
+      "smaller": "29K",
+      "webp": "20K",
+      "original_bytes": 122944,
+      "smaller_bytes": "29536",
+      "webp_bytes": 19968
+    },
+    {
+      "name": "CAP_BAY_00917025_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "65K",
+      "smaller": "17K",
+      "webp": "12K",
+      "original_bytes": 66950,
+      "smaller_bytes": "17082",
+      "webp_bytes": 11884
+    },
+    {
+      "name": "CAP_BAY_544190_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "100K",
+      "smaller": "19K",
+      "webp": "12K",
+      "original_bytes": 102530,
+      "smaller_bytes": "19736",
+      "webp_bytes": 12786
+    },
+    {
+      "name": "CAP_BLM_00888419_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "67K",
+      "smaller": "14K",
+      "webp": "8.1K",
+      "original_bytes": 68556,
+      "smaller_bytes": "14325",
+      "webp_bytes": 8276
+    },
+    {
+      "name": "CAP_BLM_00888438_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "78K",
+      "smaller": "16K",
+      "webp": "8.3K",
+      "original_bytes": 79874,
+      "smaller_bytes": "15968",
+      "webp_bytes": 8512
+    },
+    {
+      "name": "CAP_BLM_00888529_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "98K",
+      "smaller": "24K",
+      "webp": "14K",
+      "original_bytes": 100549,
+      "smaller_bytes": "24202",
+      "webp_bytes": 14134
+    },
+    {
+      "name": "CAP_BLM_00888957_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "91K",
+      "smaller": "18K",
+      "webp": "9.4K",
+      "original_bytes": 92723,
+      "smaller_bytes": "18235",
+      "webp_bytes": 9634
+    },
+    {
+      "name": "CAP_BLM_00888962_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "77K",
+      "smaller": "15K",
+      "webp": "7.9K",
+      "original_bytes": 79092,
+      "smaller_bytes": "15012",
+      "webp_bytes": 8058
+    },
+    {
+      "name": "CAP_BLM_426552_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "47K",
+      "smaller": "14K",
+      "webp": "6.2K",
+      "original_bytes": 47654,
+      "smaller_bytes": "14171",
+      "webp_bytes": 6354
+    },
+    {
+      "name": "CAP_BLM_426950_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "70K",
+      "smaller": "16K",
+      "webp": "8.7K",
+      "original_bytes": 71258,
+      "smaller_bytes": "16384",
+      "webp_bytes": 8942
+    },
+    {
+      "name": "CAP_BLM_433370_THUMBNAIL_compressed",
+      "ext": "png",
+      "original": "925K",
+      "smaller": "24K",
+      "webp": "16K",
+      "original_bytes": 947627,
+      "smaller_bytes": "24572",
+      "webp_bytes": 16104
+    },
+    {
+      "name": "CAP_CAN_00808858_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "181K",
+      "smaller": "40K",
+      "webp": "30K",
+      "original_bytes": 184965,
+      "smaller_bytes": "41275",
+      "webp_bytes": 30450
+    },
+    {
+      "name": "CAP_CAN_00808896_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "134K",
+      "smaller": "26K",
+      "webp": "18K",
+      "original_bytes": 137633,
+      "smaller_bytes": "27106",
+      "webp_bytes": 18608
+    },
+    {
+      "name": "CAP_CAN_00808901_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "162K",
+      "smaller": "33K",
+      "webp": "25K",
+      "original_bytes": 166199,
+      "smaller_bytes": "34079",
+      "webp_bytes": 25620
+    },
+    {
+      "name": "CAP_CAN_00809305_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "75K",
+      "smaller": "17K",
+      "webp": "11K",
+      "original_bytes": 76644,
+      "smaller_bytes": "17802",
+      "webp_bytes": 11202
+    },
+    {
+      "name": "CAP_CAN_00809326_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "131K",
+      "smaller": "26K",
+      "webp": "20K",
+      "original_bytes": 134639,
+      "smaller_bytes": "26603",
+      "webp_bytes": 20208
+    },
+    {
+      "name": "CAP_CAN_176424_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "199K",
+      "smaller": "38K",
+      "webp": "34K",
+      "original_bytes": 203433,
+      "smaller_bytes": "38523",
+      "webp_bytes": 34682
+    },
+    {
+      "name": "CAP_CAN_176425_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "218K",
+      "smaller": "30K",
+      "webp": "21K",
+      "original_bytes": 223281,
+      "smaller_bytes": "30931",
+      "webp_bytes": 21504
+    },
+    {
+      "name": "CAP_CAN_176426_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "140K",
+      "smaller": "23K",
+      "webp": "13K",
+      "original_bytes": 143673,
+      "smaller_bytes": "23428",
+      "webp_bytes": 13524
+    },
+    {
+      "name": "CAP_CAN_176429_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "106K",
+      "smaller": "27K",
+      "webp": "18K",
+      "original_bytes": 108263,
+      "smaller_bytes": "27683",
+      "webp_bytes": 18794
+    },
+    {
+      "name": "CAP_CAN_176430_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "66K",
+      "smaller": "15K",
+      "webp": "7.8K",
+      "original_bytes": 67928,
+      "smaller_bytes": "15172",
+      "webp_bytes": 8022
+    },
+    {
+      "name": "CAP_CAN_176431_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "84K",
+      "smaller": "21K",
+      "webp": "11K",
+      "original_bytes": 86087,
+      "smaller_bytes": "21158",
+      "webp_bytes": 11682
+    },
+    {
+      "name": "CAP_CAN_176432_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "122K",
+      "smaller": "26K",
+      "webp": "17K",
+      "original_bytes": 125137,
+      "smaller_bytes": "26640",
+      "webp_bytes": 17546
+    },
+    {
+      "name": "CAP_CAN_176433_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "104K",
+      "smaller": "20K",
+      "webp": "13K",
+      "original_bytes": 106162,
+      "smaller_bytes": "20042",
+      "webp_bytes": 13466
+    },
+    {
+      "name": "CAP_CAN_176434_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "132K",
+      "smaller": "27K",
+      "webp": "20K",
+      "original_bytes": 134974,
+      "smaller_bytes": "27666",
+      "webp_bytes": 20428
+    },
+    {
+      "name": "CAP_CAN_176435_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "202K",
+      "smaller": "33K",
+      "webp": "27K",
+      "original_bytes": 207316,
+      "smaller_bytes": "33493",
+      "webp_bytes": 27520
+    },
+    {
+      "name": "CAP_CAN_176436_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "140K",
+      "smaller": "23K",
+      "webp": "13K",
+      "original_bytes": 143673,
+      "smaller_bytes": "23428",
+      "webp_bytes": 13524
+    },
+    {
+      "name": "CAP_CAN_176437_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "66K",
+      "smaller": "15K",
+      "webp": "7.8K",
+      "original_bytes": 67928,
+      "smaller_bytes": "15172",
+      "webp_bytes": 8022
+    },
+    {
+      "name": "CAP_CAN_472670_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "176K",
+      "smaller": "29K",
+      "webp": "22K",
+      "original_bytes": 180242,
+      "smaller_bytes": "29740",
+      "webp_bytes": 22546
+    },
+    {
+      "name": "CAP_CIN_00891253_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "102K",
+      "smaller": "21K",
+      "webp": "13K",
+      "original_bytes": 104362,
+      "smaller_bytes": "21684",
+      "webp_bytes": 13496
+    },
+    {
+      "name": "CAP_CIN_00891254_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "72K",
+      "smaller": "16K",
+      "webp": "8.7K",
+      "original_bytes": 73668,
+      "smaller_bytes": "16211",
+      "webp_bytes": 8910
+    },
+    {
+      "name": "CAP_CIN_00891255_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "188K",
+      "smaller": "31K",
+      "webp": "25K",
+      "original_bytes": 192732,
+      "smaller_bytes": "31305",
+      "webp_bytes": 25674
+    },
+    {
+      "name": "CAP_CIN_00891257_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "104K",
+      "smaller": "18K",
+      "webp": "11K",
+      "original_bytes": 106204,
+      "smaller_bytes": "18714",
+      "webp_bytes": 11456
+    },
+    {
+      "name": "CAP_CIN_00891263_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "152K",
+      "smaller": "34K",
+      "webp": "29K",
+      "original_bytes": 155181,
+      "smaller_bytes": "35239",
+      "webp_bytes": 29966
+    },
+    {
+      "name": "CAP_CIN_00891270_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "105K",
+      "smaller": "19K",
+      "webp": "11K",
+      "original_bytes": 107772,
+      "smaller_bytes": "19119",
+      "webp_bytes": 11410
+    },
+    {
+      "name": "CAP_CIN_00918967_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "195K",
+      "smaller": "26K",
+      "webp": "20K",
+      "original_bytes": 199903,
+      "smaller_bytes": "27119",
+      "webp_bytes": 20788
+    },
+    {
+      "name": "CAP_CIN_00918989_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "190K",
+      "smaller": "39K",
+      "webp": "32K",
+      "original_bytes": 194512,
+      "smaller_bytes": "40020",
+      "webp_bytes": 32364
+    },
+    {
+      "name": "CAP_CIN_00918991_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "200K",
+      "smaller": "33K",
+      "webp": "27K",
+      "original_bytes": 204754,
+      "smaller_bytes": "33981",
+      "webp_bytes": 27184
+    },
+    {
+      "name": "CAP_CIN_00918992_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "125K",
+      "smaller": "21K",
+      "webp": "14K",
+      "original_bytes": 127530,
+      "smaller_bytes": "21058",
+      "webp_bytes": 14146
+    },
+    {
+      "name": "CAP_CIN_00918995_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "167K",
+      "smaller": "24K",
+      "webp": "18K",
+      "original_bytes": 171491,
+      "smaller_bytes": "24767",
+      "webp_bytes": 18622
+    },
+    {
+      "name": "CAP_CIN_00918997_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "203K",
+      "smaller": "50K",
+      "webp": "46K",
+      "original_bytes": 207371,
+      "smaller_bytes": "51344",
+      "webp_bytes": 46720
+    },
+    {
+      "name": "CAP_CIN_00918998_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "105K",
+      "smaller": "24K",
+      "webp": "16K",
+      "original_bytes": 107836,
+      "smaller_bytes": "24544",
+      "webp_bytes": 16818
+    },
+    {
+      "name": "CAP_CIN_00918999_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "82K",
+      "smaller": "20K",
+      "webp": "13K",
+      "original_bytes": 83627,
+      "smaller_bytes": "20554",
+      "webp_bytes": 13648
+    },
+    {
+      "name": "CAP_CIN_00919000_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "50K",
+      "smaller": "9.9K",
+      "webp": "5.5K",
+      "original_bytes": 50904,
+      "smaller_bytes": "10180",
+      "webp_bytes": 5682
+    },
+    {
+      "name": "CAP_CIN_00920667_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "64K",
+      "smaller": "15K",
+      "webp": "8.1K",
+      "original_bytes": 65196,
+      "smaller_bytes": "15387",
+      "webp_bytes": 8328
+    },
+    {
+      "name": "CAP_CIN_00932196_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "124K",
+      "smaller": "20K",
+      "webp": "13K",
+      "original_bytes": 127007,
+      "smaller_bytes": "20656",
+      "webp_bytes": 13728
+    },
+    {
+      "name": "CAP_CIN_00932244_THUMBNAIL",
+      "ext": "jpg",
+      "original": "134K",
+      "smaller": "23K",
+      "webp": "16K",
+      "original_bytes": 137065,
+      "smaller_bytes": "23662",
+      "webp_bytes": 16434
+    },
+    {
+      "name": "CAP_CIN_00932259_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "155K",
+      "smaller": "31K",
+      "webp": "24K",
+      "original_bytes": 158822,
+      "smaller_bytes": "31685",
+      "webp_bytes": 24394
+    },
+    {
+      "name": "CAP_CIN_00932261_THUMBNAIL_compressed",
+      "ext": "jpg",
+      "original": "152K",
+      "smaller": "30K",
+      "webp": "22K",
+      "original_bytes": 155502,
+      "smaller_bytes": "30961",
+      "webp_bytes": 22492
+    }
+  ]
+export { images }
